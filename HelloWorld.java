dd7fcc4c1fbc72bdcc6b64c015138d5bf09cef18
ironman007 <houpfchn@gmail.com>
2017-03-07 11:46:13 +0800

    添加了一个新的方法：run()

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,9 @@ public class HelloWorld{
 	public String show(){
 		return "你好世界!";	
 	}
+	public void run(){
+		System.out.println("CPU正在高速运算着！");
+	}
 	public static void main(String[] arg0s){
 		HelloWorld hello=new HelloWorld();
 		System.out.println(hello.show());

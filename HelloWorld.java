27a44ed77defeeb9b97a4530330c124e69b90623
ironman007 <houpfchn@gmail.com>
2017-03-07 11:38:53 +0800

    add new file is helloworld.java

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld{
+	public String show(){
+		return "你好世界!";	
+	}
+	public static void main(String[] arg0s){
+		HelloWorld hello=new HelloWorld();
+		System.out.println(hello.show());
+	}
+}
